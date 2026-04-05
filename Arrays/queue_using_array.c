8c1b32a334ba2701a218ef8a674ac98fe34257e3
GreatTurnip <adib9495drmc@gmail.com>
2026-04-05 17:13:23 +0600

    completed queue using array

--- Arrays/queue_using_array.c
+++ Arrays/queue_using_array.c
@@ -6,7 +6,7 @@ int front = -1; //set a flag -1 for both frond and end
 int rear = -1;
 void insert(int value)
 {
-    if(front=-1) //no queue exists till now
+    if(front==-1) //no queue exists till now
     {
         front = rear = 0; //update both
         Queue[rear] = value;
@@ -20,7 +20,24 @@ void insert(int value)
     }
     else printf("Full Queue");
 }
+void delete() //queue deletion starts from the front
+{
+    if(front==-1 && rear==-1)   printf("Empty Queue \n");
+    else if(front>=0&&front==rear)  //only one element
+    {
+        printf("deleted element : %d",Queue[front]);
+        front = rear = -1;
+    }
+    else    printf("deleted element: %d",Queue[front++]); // IMPORTANT front must be updated(incremented)
+}
 int main()
 {
+    insert(10);
+    insert(20);
+    insert(30);
 
+    delete();
+    delete();
+
+    return 0;
 }
